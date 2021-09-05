7ccd6008025aa47a6c9c4093bd7a045b25777925
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 15:57:00 +0530

    ADDED: implementation of calculatePrice in main

--- src/com/bridgelabz/inventorymanagement/InventoryMain.java
+++ src/com/bridgelabz/inventorymanagement/InventoryMain.java
@@ -6,7 +6,10 @@ public class InventoryMain {
 		Inventory rice=new Inventory("Rice",40.5, 38.8);
 		Inventory wheat=new Inventory("Wheat",23.6, 17.5);
 		Inventory pulses=new Inventory("Rice",10.4, 30.5);
-
+		
+		System.out.println("Value of total "+rice.getName()+" is "+rice.calculatePrice());
+		System.out.println("Value of total "+wheat.getName()+" is "+wheat.calculatePrice());
+		System.out.println("Value of total "+pulses.getName()+" is "+pulses.calculatePrice());
 		
 	}
 
